a382d3d297a2aeba74fb252bcd813e3f7933c72e
Carl Alldis <carlalldis@gmail.com>
2020-12-17 21:52:05 +0000

    Added a tally function

--- ChatbotController.cs
+++ ChatbotController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using ZOOM_SDK_DOTNET_WRAP;
+using System.Linq;
 
 namespace zoom_sdk_demo
 {
@@ -12,6 +13,7 @@ namespace zoom_sdk_demo
         private bool disposedValue;
         private IMeetingChatControllerDotNetWrap _chatController; // The chat controller for the zoom meeting
         private List<Message> _messages; // A list of initiative values for the round (clears every round)
+        private Dictionary<string, List<int>> _tally; // A list of initiative values per person for the session
         private int _round; // The number of this round
         private string _userName; // The username of the bot (to mitigate self-replies)
         private bool _inProgress; // Determines if there is a round in progress
@@ -19,6 +21,7 @@ namespace zoom_sdk_demo
         public ChatbotController(IMeetingChatControllerDotNetWrap chatController, string userName)
         {
             _messages = new List<Message>();
+            _tally = new Dictionary<string, List<int>>();
             _round = 1; // Start at round 1
             _inProgress = false; // Start with a round not in progress
             _userName = userName;
@@ -44,6 +47,9 @@ namespace zoom_sdk_demo
                         break;
                     case "done":
                         CompleteInitiative();
+                        break;
+                    case "tally":
+                        TallyInitiative();
                         break;
                     default:
                         AddMessage(timestamp, sender, content);
@@ -95,6 +101,46 @@ namespace zoom_sdk_demo
             }
         }
 
+        private void AddTally(string Sender, int Roll)
+        {
+            if(_tally.ContainsKey(Sender))
+            {
+                _tally[Sender].Add(Roll);
+            }
+            else
+            {
+                var newList = new List<int>();
+                newList.Add(Roll);
+                _tally.Add(Sender, newList);
+            }
+        }
+
+        private void TallyInitiative()
+        {
+            var tallyTotals = new List<Tally>();
+            foreach (var sender in _tally.Keys)
+            {
+                var rolls = _tally[sender];
+                var newTally = new Tally()
+                {
+                    Sender = sender,
+                    Rolls = _tally[sender],
+                    Total = rolls.Sum()
+                };
+                tallyTotals.Add(newTally);
+            }
+            tallyTotals.Sort();
+            var resultTallyList = new List<String>();
+            resultTallyList.Add("Initiative tally:");
+            resultTallyList.Add("");
+            foreach (var tally in tallyTotals)
+            {
+                resultTallyList.Add(tally.Sender + ": " + tally.Total + " (" + string.Join(", ", tally.Rolls) + ")");
+            }
+            var resultTally = string.Join("\n", resultTallyList);
+            SendMessageEveryone(resultTally);
+        }
+
         /// <summary>
         /// Add a new value to the round in progress
         /// </summary>
@@ -115,6 +161,7 @@ namespace zoom_sdk_demo
                         Roll = roll,
                     };
                     _messages.Add(message);
+                    AddTally(message.Sender, message.Roll);
                 }
                 catch
                 {
@@ -161,6 +208,18 @@ namespace zoom_sdk_demo
         public int CompareTo(Message other)
         {
             return other.Roll.CompareTo(Roll);
+        }
+    }
+
+    class Tally : IComparable<Tally>
+    {
+        public string Sender { get; set; }
+        public int Total { get; set; }
+        public List<int> Rolls { get; set; }
+
+        public int CompareTo(Tally other)
+        {
+            return other.Total.CompareTo(Total);
         }
     }
 }
